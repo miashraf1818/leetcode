56f2e7c26834eb550a183e529f97ed9a7d5fb1b4
Mohammed Ikram Ashrafi <112676762+miashraf1818@users.noreply.github.com>
2026-09-02 23:31:56 +0530

    Time: 0 ms (100%), Space: 19.3 MB (47.47%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
@@ -0,0 +1,7 @@
+from typing import List
+
+class Solution:
+    def uniformArray(self, nums1: List[int]) -> bool:
+        # As proven by parity combinations, a valid uniform array 
+        # can mathematically always be constructed for any input.
+        return True

67eb4a3ebc782f7a90b9fadc6573785d83dd5ada
Mohammed Ikram Ashrafi <112676762+miashraf1818@users.noreply.github.com>
2026-09-04 20:15:11 +0530

    Time: 4 ms (58.13%), Space: 19.4 MB (9.85%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
@@ -0,0 +1,22 @@
+class Solution:
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+        from typing import List
+        n = len(nums)
+        
+        # Precompute the minimums from index i to the end of the array
+        suf_min = [0] * n
+        suf_min[-1] = nums[-1]
+        for i in range(n - 2, -1, -1):
+            suf_min[i] = min(nums[i], suf_min[i + 1])
+            
+        pref_max = float('-inf')
+        
+        # Iterate to find the smallest stable index
+        for i in range(n):
+            pref_max = max(pref_max, nums[i])
+            instability_score = pref_max - suf_min[i]
+            
+            if instability_score <= k:
+                return i
+                
+        return -1
